5512f4c893709d8161bb8b360665f9cbf3eb2189
andormade <hngrhorace@gmail.com>
2020-10-12 22:58:06 +0200

    fix: rounding errors

--- src/webcomponents/LocalizedTemperature.js
+++ src/webcomponents/LocalizedTemperature.js
@@ -1,6 +1,6 @@
 class LocalizedTemperature extends HTMLElement {
 	connectedCallback() {
-		const celsius = parseInt(this.getAttribute('celsius'), 10);
+		const celsius = parseFloat(this.getAttribute('celsius'));
 		const unit = this.getAttribute('unit') || 'metric';
 		const fahrenheit = (celsius * 9) / 5 + 32;
 
@@ -9,6 +9,7 @@ class LocalizedTemperature extends HTMLElement {
 				this.innerHTML = `${celsius} °C`;
 				break;
 			case 'imperial':
+			case 'us':
 				this.innerHTML = `${fahrenheit} °F`;
 				break;
 			default:
--- src/webcomponents/LocalizedVolume.js
+++ src/webcomponents/LocalizedVolume.js
@@ -1,17 +1,22 @@
+const round2dec = num => Math.round((num + Number.EPSILON) * 100) / 100;
+
 class LocalizedVolume extends HTMLElement {
 	connectedCallback() {
 		const multiplier = parseFloat(this.getAttribute('multiplier')) || 1;
-		const ml = parseInt(this.getAttribute('ml'), 10) * multiplier;
+		const ml = parseFloat(this.getAttribute('ml')) * multiplier;
 		const unit = this.getAttribute('unit') || 'metric';
-		const oz = (ml / 29.574) ;
-
+		const us_oz = round2dec(ml / 29.5735295625);
+		const imperial_oz = round2dec(ml / 28.4130625);
 
 		switch (unit) {
 			case 'metric':
 				this.innerHTML = `${ml} ml`;
 				break;
 			case 'imperial':
-				this.innerHTML = `${oz} oz`;
+				this.innerHTML = `${imperial_oz} Imp fl oz`;
+				break;
+			case 'us':
+				this.innerHTML = `${us_oz} US fl oz`;
 				break;
 			default:
 				this.innerHTML = `${ml}`;
